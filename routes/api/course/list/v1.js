57d29e70a20280c03d57599b036fd9bf713b439e
Noel Sicoco <nmsicoco@gmail.com>
2015-09-22 17:10:52 -0400

    Added course/list/v1 api call

--- routes/api/course/list/v1.js
+++ routes/api/course/list/v1.js
@@ -15,18 +15,22 @@ var router = express.Router();
 /////////////////////////
 
 /**
- * @api {get} /api/department/list/v1 List departments
- * @apiName ListDepartmentV1
- * @apiGroup Department
+ * @api {get} /api/course/list/v1 List courses
+ * @apiName ListCourseV1
+ * @apiGroup Course
  *
- * @apiSuccess (200) {Object[]} departments       List of departments
- * @apiSuccess (200) {String}   departments.id    3-character department id
- * @apiSuccess (200) {String}   departments.name  Name of department
+ * @apiSuccess (200) {Object[]} courses               List of courses
+ * @apiSuccess (200) {String}   courses.coursecode    6-character course code
+ * @apiSuccess (200) {String}   courses.name          Name of course
+ * @apiSuccess (200) {String}   courses.description   Description of course
  */
 router.get("/",
   function(req, res) {
-    var psName = "api_department_list_v1";  // Prepared Statement Name
-    var query = "SELECT * FROM department";
+    var psName = "api_course_list_v1";  // Prepared Statement Name
+    var query =
+      "SELECT crscode AS coursecode, name, descr AS description " +
+      "FROM course " +
+      "ORDER BY coursecode";
     var values = [];
     dbHelper.query(psName, query, values, function(err, result) {
       if(err) {
@@ -34,7 +38,7 @@ router.get("/",
         logger.error(err);
       } else {
         res.json({
-          departments: result.rows
+          courses: result.rows
         });
       }
     });
